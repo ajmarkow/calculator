484c83ad467c6db56891c067e7feb7c7940d5e20
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 14:04:44 -0700

    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com> Co-authored by: AJ Markow <alexanderjmarkow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-function add(number1, number2) {
+function add(number1, number2) { 
+  parseInt(number1);
+  parseInt(number2);
   return number1 + number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+let result = add(number1, number2);
 alert(result);
 
 function multiply(number1, number2){
-  parseInt(number1);
-  parseInt(number2);
+ 
   return number1 * number2;
 }
 
