06048825b070ec6ee4d0a277fb5009c96813bf7b
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 13:14:14 -0700

    Initial copied version

---
+++ otherscripts.js
@@ -0,0 +1,5 @@
+function convertTemperature(CelsiusTemperature){
+  const CelsiusTemperature =parseInt(prompt("Enter a temperature:"));
+  const result = convertTemperature(CelsiusTemperature);
+}
+alert (result);
