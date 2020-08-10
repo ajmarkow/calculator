cc29cd26dc45b54f4e065f096228749663c268da
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 13:00:21 -0700

    initial functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 const result = add(number1, number2);
 alert(result);
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const resultMulti = multiply(number1, number2);
+alert(resultMulti);
+
+function subtract(number1, number2){
+  return number1-number2
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt(Enter anothernumber)); 
+const resultsubtract=subtract(number1,number2)
