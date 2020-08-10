d9201f5e8b488e7fc4540d8c7f56e08e90ee84f0
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 13:29:14 -0700

    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com> Co-authored by: AJ Markow <alexanderjmarkow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,23 @@ function add(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = multiply(number1, number2);
+const result = add(number1, number2);
 alert(result);
 
 function multiply(number1, number2){
+  parseInt(number1);
+  parseInt(number2);
   return number1 * number2;
 }
 
 function subtract(number1, number2){
-  parseInt(number1)
+  parseInt(number1);
   parseInt(number2);
   return number1-number2
 }
 
 function divide(number1,number2){
-parseInt(number1)
+parseInt(number1);
 parseInt(number2);
 return number1/number2
 }
