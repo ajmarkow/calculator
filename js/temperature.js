446af350b4a25e67283995517f70a9f37f3877d5
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-10 14:06:05 -0700

    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com> Co-authored by: AJ Markow <alexanderjmarkow@gmail.com>
    Add temperature convert function.

--- js/temperature.js
+++ js/temperature.js
@@ -1,6 +1,11 @@
-function convertTemperature(CelsiusTemperature){
-  let CelsiusTemperature = parseInt(prompt("Enter a temperature:"));
-  result = ((CelsiusTemperature*1.8+32));
-}
+const number1 = parseInt(prompt("Enter a number:"));
 
-alert (result);
+let result= convert(number1);
+alert(result);
+
+function convert(number1){
+  parseFloat(number1);
+  
+  return number1 * 1.8 +32;
+  
+}
