6eee34d3cbccd9b7c1517829cfc24fcd4095ff63
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-12 10:44:39 -0700

    Add user interface logic for addition
    Co-authored by: Jaki Widby <jakiwidby@gmail.com>
    Co-authored by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
-function add(number1, number2) { 
+// Businesss logic goes here:
+
+function add(number1, number2) {
   parseInt(number1);
   parseInt(number2);
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-let result = add(number1, number2);
-alert(result);
+function multiply(number1, number2) {
 
-function multiply(number1, number2){
- 
   return number1 * number2;
 }
 
-function subtract(number1, number2){
+function subtract(number1, number2) {
   parseInt(number1);
   parseInt(number2);
-  return number1-number2
+  return number1 - number2
 }
 
-function divide(number1,number2){
-parseInt(number1);
-parseInt(number2);
-return number1/number2
+function divide(number1, number2) {
+  parseInt(number1);
+  parseInt(number2);
+  return number1 / number2
 }
 //const resultMulti = multiply(number1, number2);
 //alert(resultMulti);
+
+//User interface logic here:
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
 
 
