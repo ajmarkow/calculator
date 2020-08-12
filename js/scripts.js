b4cbf20cb3a4da16fd9b4db236234774d8f50e40
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-12 11:14:00 -0700

    Add user interface logic for multiplication, division,subtraction. Update result style using CSS.
    Co-authored by: Jaki Widby <jakiwidby@gmail.com>
    Co-authored by: Joseph Nilles <jbnilles24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,18 @@
 // Businesss logic goes here:
 
 function add(number1, number2) {
-  parseInt(number1);
-  parseInt(number2);
   return number1 + number2;
 }
 
 function multiply(number1, number2) {
-
   return number1 * number2;
 }
 
 function subtract(number1, number2) {
-  parseInt(number1);
-  parseInt(number2);
   return number1 - number2
 }
 
 function divide(number1, number2) {
-  parseInt(number1);
-  parseInt(number2);
   return number1 / number2
 }
 //const resultMulti = multiply(number1, number2);
@@ -29,9 +22,30 @@ function divide(number1, number2) {
 $(document).ready(function () {
   $("form#add").submit(function (event) {
     event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+    const number1 = parseFloat($("#add1").val());
+    const number2 = parseFloat($("#add2").val());
     const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseFloat($("#sub1").val());
+    const number2 = parseFloat($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#divide").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseFloat($("#div1").val());
+    const number2 = parseFloat($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#multiply").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseFloat($("#mult1").val());
+    const number2 = parseFloat($("#mult2").val());
+    const result = multiply(number1, number2);
     $("#output").text(result);
   });
 });
